1506c58c526759698266aadc55413289a06ceca1
daxit <daxitm2112@gmail.com>
2023-08-28 14:27:30 +0530

    change position of downloadBox

--- frontend/src/components/header/HeaderBar.vue
+++ frontend/src/components/header/HeaderBar.vue
@@ -1,28 +1,28 @@
 <template>
-  <header>
-    <!-- <img v-if="showLogo !== undefined" :src="logoURL" />
+  <div class="headerContainer">
+    <header>
+      <!-- <img v-if="showLogo !== undefined" :src="logoURL" />
     <slot /> -->
-    
-    
-    <div>
-      <div id="dropdown" :class="{ active: this.$store.state.show === 'more' }">
-      <slot name="actions" />
-    </div>
 
-    <action v-if="this.$slots.actions" id="more" icon="more_vert" :label="$t('buttons.more')"
-      @action="$store.commit('showHover', 'more')" />
 
-    <div class="overlay" v-show="this.$store.state.show == 'more'" @click="$store.commit('closeHovers')" />
+      <div>
+        <div id="dropdown" :class="{ active: this.$store.state.show === 'more' }">
+          <slot name="actions" />
+        </div>
 
-    </div>
-  
+        <action v-if="this.$slots.actions" id="more" icon="more_vert" :label="$t('buttons.more')"
+          @action="$store.commit('showHover', 'more')" />
+
+        <div class="overlay" v-show="this.$store.state.show == 'more'" @click="$store.commit('closeHovers')" />
+
+      </div>
+
+    </header>
     <div class="downloadBox">
       <input v-model="downloadLink" placeholder="https://civitai.com/api/download/models/143906" />
       <button @click="downloadFile">Download</button>
     </div>
-
-
-  </header>
+  </div>
 </template>
 
 <script>
@@ -47,7 +47,7 @@ export default {
       this.$store.commit("showHover", "sidebar");
     },
     async downloadFile() {
-      
+
       try {
         this.$toast.info('Downloading...');
         await api.downloadFile(this.$route.path, this.downloadLink);
@@ -56,7 +56,7 @@ export default {
       } catch (error) {
         this.$toast.error('An error occurred while downloading!');
       }
-        
+
     },
   },
 };
